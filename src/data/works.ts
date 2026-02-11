15c22310a0b3fa1ac06b3515cc4ff4dc544a2833
Yu-chan_141 <206151013+Yuichiro-ISHII-141@users.noreply.github.com>
2026-02-11 19:27:54 +0900

    Add new 3D model to gallery: work-002 and update description of work-001

--- src/data/works.ts
+++ src/data/works.ts
@@ -14,8 +14,16 @@ export const works: Work[] = [
   {
     id: "work-001",
     title: "001: 馬の人形",
-    description: "五月人形として飾っていた馬の人形です。",
+    description: "馬の人形です。子供の頃、実家で五月人形のお供として飾っていたものです。",
     tags: ["sample"],
     splatSrc: `${BASE}splats/work_001.compressed.ply`,
+  },
+
+    {
+    id: "work-002",
+    title: "002: 黒松の盆栽",
+    description: "私が育てている黒松の盆栽です。植え替え前なので、まだビニールポットに植えられています。",
+    tags: ["sample"],
+    splatSrc: `${BASE}splats/work_002.compressed.ply`,
   },
 ];
